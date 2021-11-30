51be934df4decf0291976f3cad963c0da647c773
ThatOneFoxx <81937424+ThatOneFoxx@users.noreply.github.com>
2021-11-30 08:53:54 -0600

    Create index.php
    First draft with no working api

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$files = glob('../images/*');
+if ($files) {
+    $Number = count($files);
+    $index = rand(1, $Number);
+
+    $image_path = '';
+    $link = ';
+} else {
+    $image_path = null;
+    $link = null;
+}
+$data = ['image' => $image_path, 'link' => $link];
+
+echo json_encode($data);
